62e6f0f5baa86040364e36ee8451854050a2dc56
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-06-25 08:00:29 +0300

    chart descriptions and paths added

--- charts-month-year.php
+++ charts-month-year.php
@@ -1,15 +1,16 @@
 <?php include('livedata.php');
-// pure csv without mysql extras built September 2018//
+// weather34 pure csv without mysql extras built September 2018 finished June 2019//
 date_default_timezone_set($TZ);
 $date = date_create();
 $year=date('Y');
 //disable error logging if you get server errors
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);ini_set("display_errors","off");
-//$weatherchartfile = "result2.txt";
+//path is weathercharts/year/month.csv (eg: weathercharts/2019/June.csv )
 $weatherchartfilemonth ="weather34charts/".$year."/".date('M').".csv";
+//path is weathercharts/year.csv (eg: weathercharts/2019.csv )
 $weatherchartfileyear = "weather34charts/".date('Y').".csv";
 //(date),MAX(outsideTemp),MIN(outsideTemp),MAX(dewpoint),MIN(dewpoint),MAX(raintoday),MAX(windspeed),ROUND(AVG(windSpeed),2),MAX(barometer),MIN(barometer)
-$id=0;
+//$id=0;
 //create the current month file
 $weather34chartdata   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[30].",".$meteobridgeapi[158].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
 $output=$weatherchartfilemonth;
--- chartstoday.php
+++ chartstoday.php
@@ -1,11 +1,12 @@
 <?php include('livedata.php');
-// extras added November 2017//
+// weather34 pure csv without mysql extras built September 2018 finished June 2019//
 date_default_timezone_set($TZ);
 $date = date_create();
 $id=00;
 //disable error logging if you get server errors
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);ini_set("display_errors","off");
-$weatherchartfile = "weather34charts/".date('jMY').".csv";
+//path is weathercharts/year/day.csv (eg: weathercharts/2019/24Jun2019.csv )
+$weatherchartfile = "weather34charts/".date('Y')."/".date('jMY').".csv";
 //ID,time,outsideTemp,barometer,raintoday,UV,windgustmph,windSpeedAvg,radiation,dewpoint,rainrate,direction,date,indoortemp,indoorhumidity
 $weather34chartdata   = $id.",".date('H:i').",".$meteobridgeapi[2].",".$meteobridgeapi[10].",". $meteobridgeapi[9].",".$meteobridgeapi[43].",".$meteobridgeapi[6].",".$meteobridgeapi[5].",".$meteobridgeapi[45].",".$meteobridgeapi[4].",".$meteobridgeapi[8].",".$meteobridgeapi[7].",".$meteobridgeapi[22].",".$meteobridgeapi[23].",".date('j M').""."\r\n";
 $output=$weatherchartfile;
@@ -13,5 +14,3 @@ $fp = fopen($weatherchartfile, 'a');
 fwrite($fp,$weather34chartdata); 
 fclose($weatherchartfile);
 ?>
-
-
