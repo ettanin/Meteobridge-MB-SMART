8de744de11afc7c2c9ef38261c2dd6bc1755164e
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-07-24 05:41:34 +0300

    Finalize Files

--- barometer.php
+++ barometer.php
@@ -25,7 +25,7 @@ transform:rotate(<?php if ($weather["barometer_units"]=='mb' OR $weather["barome
 .weather34barometerarrowmax{-webkit-transform:rotate(<?php 
 if ($weather["barometer_units"]=='mb' OR $weather["barometer_units"]=="hPa" ){echo $weather["barometer_max"]*0.02953*50.6;}else if ($weather["barometer_units"]=='inHg'){echo $weather["barometer_max"]*50.6;}?>deg);
 transform:rotate(<?php if ($weather["barometer_units"]=='mb' OR $weather["barometer_units"]=="hPa" ){echo $weather["barometer_max"]*0.02953*50.6;}else if ($weather["barometer_units"]=='inHg'){echo $weather["barometer_max"]*50.6;}?>deg);}
-valuetextba{font-size:1em}
+valuetextba{font-size:1em}valuetextbc{font-size:.8em}
 </style>
 <div class="moduleupdatetime"><span><?php if(file_exists($livedata)&&time()- filemtime($livedata)>300)echo '<offlinenew></offlinenew> Offline';else echo "<onlinenew></onlinenew> ".$weather["time"];?></div> 
 <div class='maxpressure'><a class="weather34tipmax" data-weather34tipmax="<?php echo "Max ".$weather["thb0seapressymax"]." ".$weather["barometer_units"]."&nbsp; ".$weather["thb0seapressyearmaxtime"]?>">
@@ -33,7 +33,7 @@ valuetextba{font-size:1em}
 <div class='minpressure'><a class="weather34tipmin" data-weather34tipmin="<?php echo "Min ".$weather["thb0seapressymin"]." ".$weather["barometer_units"]."&nbsp; ".$weather["thb0seapressyearmintime"]?>">
 <?php echo '<valuetext>Min ('.$weather["thb0seapressmintime"].')<br><minblue>',$weather["barometer_min"],'&nbsp;</minblue><pressureunits>',$weather["barometer_units"],' </pressureunits></valuetext></div>';?></a></div>
 <div class="trendpressure">
-<?php  echo "<valuetext>&nbsp;&nbsp;Trend";
+<?php  echo "<valuetextbc>&nbsp;&nbsp;Trend";
 if ($weather["barometer_trend"] > 20  && $weather["barometer_trend"] < 100)  {echo '<rise> '.$risingsymbol.' </rise><valuetextba>';echo number_format($weather["barometer_trend"],2), '</valuetextba><pressureunits> ';echo $weather["barometer_units"], '</pressureunits>';}
 else if ($weather["barometer_trend"] < 0) {echo '<fall> '.$fallingsymbol.'</fall><valuetextba>'; echo number_format($weather["barometer_trend"],2), '</valuetextba><pressureunits> ';echo $weather["barometer_units"], '</pressureunits></valuetext>';}
 else if ($weather["barometer_trend"] > 0 && $weather["barometer_trend"] < 100) {
