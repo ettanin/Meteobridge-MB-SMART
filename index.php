2c2f42cd2bad373f79790b0eee3d5ccb6e64855d
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-07-26 22:25:12 +0300

    Finalized Files(adds logo to footer)

--- index.php
+++ index.php
@@ -206,7 +206,8 @@ else if ($davis=="Yes"){echo '<img src="img/logoDAVISCONSOLEVECTOR.svg" alt="Dav
   <div class="footercontainer">
   <main class="gridfooter">
   <weather34footer>
-  <div class=footertext><?php echo $info?>&nbsp;Realtime Weather Data Provided By:&nbsp;<?php echo $stationlocation?> Home Weather Station <div class="logoweather34"><a class="weather34tipfooter" href="https://weather34.com/homeweatherstation/" data-weather34tipfooter="WEATHER34 MB-SMART" target="_blank"><img src="img/weather34-logo.svg" width=50em ></a></div></div>
- </weather34footer></main></div>
+  <div class=footertext>
+  <div class="weather34-mbsmartlogo"><a class="weather34tipfooter" href="https://weather34.com/homeweatherstation/" data-weather34tipfooter="WEATHER34 MB-SMART" target="_blank"><img src="img/weather34-logo.svg" width=47em > <span>weather<ored>34</ored> designed</span></a></div>
+  <?php echo $info?>&nbsp;Weather Data Provided By:&nbsp;<?php echo $stationlocation?> Home Weather Station </main></div>
  
  </body><?php include_once('updater.php');include_once('menu.php');?><div id=lightningalert></div>
