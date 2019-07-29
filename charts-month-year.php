5edab4cc1118df646455acf973075090e7d7a166
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-07-29 07:44:10 +0300

    Update chart putput script

--- charts-month-year.php
+++ charts-month-year.php
@@ -14,25 +14,27 @@ $date = date_create();
 $year=date('Y');
 //disable error logging if you get server errors
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);ini_set("display_errors","off");
-//path is weathercharts/year/month.csv (eg: weathercharts/2019/June.csv )
-$weatherchartfilemonth ="weather34charts/".$year."/".date('F').".csv";
-//path is weathercharts/year.csv (eg: weathercharts/2019.csv )
-$weatherchartfileyear = "weather34charts/".date('Y').".csv";
+
 
 //(date month-year-0)),MAX(Temp-1),MIN(Temp-2),MAX(Dewpoint-3),MIN(Dewpoint-4),TOTAL(Raintoday-5),MAX(Windspeed-6),AVG(Day WindSpeed-7),AVG(Temp-8),MAX(Barometer-9),MIN(Barometer-10)
 //create the current month file example folder/file path = weathercharts/2019/July.csv
+
+//path is weathercharts/year/month.csv (eg: weathercharts/2019/June.csv )
+$weatherchartfilemonth ="weather34charts/".$year."/".date('F').".csv";
 $weather34chartdata   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[32].",".$meteobridgeapi[158].",".$meteobridgeapi[123].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
-$output=$weatherchartfilemonth;
+//$output=$weatherchartfilemonth;
 $fp = fopen($weatherchartfilemonth, 'a'); 
-echo fwrite($fp,$weather34chartdata); 
+fwrite($fp,$weather34chartdata); 
 fclose($weatherchartfilemonth);
 
 //create the year file example folder/file path = weathercharts/2019.csv
 //(date month-year-0)),MAX(Temp-1),MIN(Temp-2),MAX(Dewpoint-3),MIN(Dewpoint-4),TOTAL(Raintoday-5),MAX(Windspeed-6),AVG(Day WindSpeed-7),AVG(Temp-8),MAX(Barometer-9),MIN(Barometer-10)
-$weather34chartdata   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[32].",".$meteobridgeapi[158].",".$meteobridgeapi[123].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
-$output=$weatherchartfileyear;
-$fp = fopen($weatherchartfileyear, 'a'); 
-echo fwrite($fp,$weather34chartdata); 
+//path is weathercharts/year.csv (eg: weathercharts/2019.csv )
+$weatherchartfileyear = "weather34charts/".date('Y').".csv";
+$weather34chartdata1   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[32].",".$meteobridgeapi[158].",".$meteobridgeapi[123].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
+//$output1=$weatherchartfileyear;
+$fp1 = fopen($weatherchartfileyear, 'a'); 
+fwrite($fp1,$weather34chartdata1); 
 fclose($weatherchartfileyear);
 ?>
 
