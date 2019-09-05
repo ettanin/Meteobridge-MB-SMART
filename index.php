e5ab823a864c2f481cedbe41eb98c189b9bc9ac6
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-09-05 14:57:09 +0300

    another fix ref:piet forecast links

--- index.php
+++ index.php
@@ -84,16 +84,9 @@ include_once('livedata.php');include_once('common.php');include_once('settings1.
   <div class=actualt><?php echo $lang['Forecast'] ;?>  (<valuetitleunit>&deg;<?php echo $weather["temp_units"] ;?></valuetitleunit>) </div>
   <div class=weather34chartlinks>
 
-<?php if ($position6=='forecast3wu.php' || $position6=='forecast3wularge.php') {
-    echo ' <a class="weather34tiplink" data-weather34tiplink="10 Day Forecast" href="outlookwu.php" data-lity>'. $chartinfo." ".$lang['Forecast']." </a>&nbsp;";
-}?>
-<?php if ($position6=='forecast3wu.php' || $position6=='forecast3wularge.php') {
-    echo ' <a class="weather34tiplink" data-weather34tiplink="Forecast Summary" href="outlookwutext.php" data-lity>'. $chartinfo. " ".$lang['Forecastsummary']." </a>&nbsp;";
-}?>
+<?php echo ' <a class="weather34tiplink" data-weather34tiplink="5 Day Forecast" href="outlookwu.php" data-lity>'. $chartinfo." ".$lang['Forecast']." </a>&nbsp;";?>
+<?php  echo ' <a class="weather34tiplink" data-weather34tiplink="Forecast Summary" href="outlookwutext.php" data-lity>'. $chartinfo. " ".$lang['Forecastsummary']." </a>&nbsp;";?>
 
-<?php if ($position4=='barometer-year-month.php') {
-    echo ' <a class="weather34tiplink" data-weather34tiplink="Forecast Summary" href="davisconsole.php" data-lity>'. $chartinfo. " Console 24-48hr " .$lang['Forecast']." </a>&nbsp;";
-}?>
 </span></div>
  <div id=currentfore></div></weather34module>
 
