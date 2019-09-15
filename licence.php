857e6de9d4ca4cc201515d21bc953b37856dc839
weather34 <weather34@brians-Mac-mini.local>
2019-09-15 21:31:02 +0300

    licence updated and credits added

--- licence.php
+++ licence.php
@@ -78,7 +78,6 @@ date{color:#aaa;font-size:.65em;margin-top:10px}
 <em>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Template.</em>
 <br>
 THE TEMPLATE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE TEMPLATE OR THE USE OR OTHER DEALINGS IN THE TEMPLATE.
-Copyright &copy; 2015-<?php echo date('Y')?> by Brian Underdown * <a href="https://weather34.com/homeweatherstation/" title="https://weather34.com/homeweatherstation/" target="_blank">https://weather34.com</a> 
 
 <br><a href="https://github.com/weather34/Meteobridge-MB-SMART" title="https://github.com/weather34/Meteobridge-MB-SMART" target="_blank">
 <svg id="github" viewBox="0 0 64 64" width="22" height="22"><path stroke-width="0" fill="#009bab" d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z" /></svg></a>
@@ -93,7 +92,8 @@ Lars (Singapore),Paul Wilman(UK),Graham(Guernsey),Ron(USA K1TTC),Piet(Netherland
 
 <br>
 <a href="https://weather34.com/homeweatherstation/" alt="https://weather34.com/homeweatherstation/" title="https://weather34.com/homeweatherstation/" target="_blank">
- <img src="img/weather34-mbsmart-logo.svg" alt="weather34 logo" class="weather34logo" width="40px"><white>weather<red6>34</red6>.com designed</white></a>
+ <img src="img/weather34-mbsmart-logo.svg" alt="weather34 logo" class="weather34logo" width="40px"><white>
+ Copyright &copy; 2015-<?php echo date('Y')?> by Brian Underdown * <a href="https://weather34.com/homeweatherstation/" title="https://weather34.com/homeweatherstation/" target="_blank">https://weather34.com </white>
 
 </article> 
    </main>
