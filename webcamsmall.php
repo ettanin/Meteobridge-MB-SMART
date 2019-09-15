e4a3dd13b14f5e8f425c8c2e3c644720ed808044
weather34 <weather34@brians-Mac-mini.local>
2019-09-15 10:33:47 +0300

    ref ron K1TTC change way the offline / online works for webcam

--- webcamsmall.php
+++ webcamsmall.php
@@ -3,15 +3,8 @@
 .webcam{
 -webkit-border-radius:4px;	-moz-border-radius:4px;	-o-border-radius:4px;	-ms-border-radius:4px;border-radius:4px;border:solid RGBA(84, 85, 86, 1.00) 2px;width:275px;height:145px;margin:4px;}
 </style>
-
-<?php $file_headers = @get_headers($webcamurl); ?>
 <div class="updatedtime1"><span>
-<?php if($file_headers && $file_headers[0] != 'HTTP/1.1 404 Not Found') {
-  echo $online.' '.date($timeFormat);
-} else {
-  echo $offline. '<offline> Offline </offline>';
-}
-?>
+<?php if(file_exists($livedata)&&time()- filemtime($livedata)>300)echo $offline. '<offline> Offline </offline>';else echo $online." ".$weather["time"];?>
 </span></div>
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='webcam' !!! -->
 <a href="cam.php" data-lity><img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>>" alt="weathercam" class="webcam"></a>
