b1d229a5aa5091929063e2da72413a386b9cc69d
weather34 <weather34@brians-Mac-mini.local>
2019-09-22 14:10:59 +0300

    minor rewording

--- stationinfo.php
+++ stationinfo.php
@@ -89,7 +89,7 @@ border:solid 2px #4b545c;border-bottom:solid 15px #4b545c;-webkit-border-radius:
 $nanosduptime = $meteobridgeapi[81];function convert($nanosduptime){$weather34nanotimeago = "";$days1 = intval(intval($nanosduptime) / (3600*24));$hours1 = (intval($nanosduptime) / 3600) % 24;$minutes1 = (intval($nanosduptime) / 60) % 60;$seconds1 = (intval($nanosduptime)) % 60;if($days1> 1){$weather34nanotimeago .= "$days1 Days ";}else {if($days1>0){$weather34nanotimeago .= "$days1 Day ";}if($hours1 > 1){$weather34nanotimeago .= "$hours1 hrs ";}else if($hours1 >=0){$weather34nanotimeago .= "$hours1 hr ";}if($minutes1 > 1){$weather34nanotimeago .= "$minutes1 mins ";}else if($minutes1 >=0){$weather34nanotimeago .= "$minutes1 min ";}}return $weather34nanotimeago;}?>
   
     Interface:Meteobridge <blue><?php echo $mbplatform?> </blue><?php echo $weather["mbplatform"]?><br>
-    Uptime:<blue><?php echo convert($nanosduptime)?> Ago</blue><br>
+    Interface Uptime :<blue><?php echo convert($nanosduptime)?></blue><br>
     Firmware:<blue><?php echo $weather["swversion"]."-" .$weather["build"]?></blue><br>
     <div style="position:absolute;display:inline-block;margin-left:45px;margin-top:15px;">
     <?php 
