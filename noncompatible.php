923c5b42d834208c085de97a2698be7ae6d4868c
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-11-13 12:07:06 +0300

    Update noncompatible.php

--- noncompatible.php
+++ noncompatible.php
@@ -5,17 +5,18 @@
 position:absolute;
 top: 0;
 left: 0;
-
-background-color: #000;
+background-color:#fff;
 filter:alpha(opacity=70);
 -moz-opacity:0.7;
 -khtml-opacity: 0.7;
 opacity: 0.7;
 z-index: 100;
 display: none;
+color: #333;
 }
 .cnt223 a{
 text-decoration: none;
+color: #333;
 }
 .popup{
 margin:0 auto;
@@ -23,31 +24,39 @@ display: none;
 position: fixed;
 top:25%;
 z-index: 101;
+color: #333;
+text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);
+
 }
 .cnt223{
 min-width: 300px;
 max-width: 500px;
-min-height: 150px;
+height:180px;
+min-height: 180px;
 margin:0 auto;
-
-background: #f3f3f3;
+background: white;
 position: absolute;
 z-index: 103;
-padding: 15px 35px;
-border-radius: 5px;
+padding: 15px 15px;
+border-radius: 3px;
 box-shadow: 0 2px 5px #000;
+font-size: 12px;
+color: #333;
+text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);
 }
 .cnt223 p{
 clear: both;
     color: #333;
     /* text-align: justify; */
-    font-size: 18px;
+    font-size: 12px;
     font-family: sans-serif;
     text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);
+    
 }
 .cnt223 p a{
 color: #01a4b4;
 font-weight: normal;
+text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);
 }
 .cnt223 .x{
 float: right;
@@ -56,11 +65,14 @@ left: 22px;
 position: relative;
 top: -25px;
 width: 34px;
+text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);
 }
 .cnt223 .x:hover{
 cursor: pointer;
 }
-.close{color:##202020;font-size:0.8em;}
+.close{color:#333;font-size:11px;line-height:1;width:100%;text-shadow: 1px 1px 3px rgba(61, 70, 77, 0.2);padding:3px}
+blue{color:color: #01a4b4;}
+.logo{float:right;bottom:0;width:25px;position:relative;}
 </style>
 <script type='text/javascript'>
 $(function(){
@@ -83,8 +95,8 @@ return false;
 </script>
 <div class='popup'>
 <div class='cnt223'>
-<span class='close'>
-<svg version="1.2" x="0px" y="0px"  viewBox="0 0 55.597 55.597"  width="85px">
+<div class='close'>
+<svg version="1.2" x="0px" y="0px"  viewBox="0 0 55.597 55.597"  width="35px">
 <path style="fill:#5C6887;" d="M55.458,51.807c0.389,0.664-0.09,1.5-0.859,1.5H27.625H0.997c-0.767,0-1.246-0.831-0.862-1.495
 	L26.768,2.787c0.382-0.661,1.335-0.663,1.721-0.005L55.458,51.807z"/>
 <polygon style="fill:#fa544d;" points="8.207,47.45 27.659,11.112 47.357,47.45 "/>
@@ -94,16 +106,20 @@ return false;
 	<path style="fill:#FFFFFF;" d="M27.597,40.307c-0.553,0-1,0.447-1,1v2c0,0.553,0.447,1,1,1s1-0.447,1-1v-2
 		C28.597,40.754,28.149,40.307,27.597,40.307z"/>
 </g></svg>
-</span>
-<p>
+</div>
+
 Sorry your hardware or software interface is not compatible with this Weather34 designed template MB-SMART.
 <br/>
-<br/>
-<span class='close'>Supports only Meteobridge 
-<a href="https://www.meteobridge.com/wiki/index.php/Meteobridge_NANO_SD" target="_blank" title="https://www.meteobridge.com/wiki/index.php/Meteobridge_NANO_SD">NANO(SD)</a> 
+
+<div class='close'>Supports only 
+<br>Meteobridge 
+<a href="https://www.meteobridge.com/wiki/index.php/Meteobridge_NANO_SD" target="_blank" title="https://www.meteobridge.com/wiki/index.php/Meteobridge_NANO_SD"><blue>NANO(SD)</blue></a> 
 or 
-<a href="https://www.meteobridge.com/wiki/index.php/Meteobridge_PRO" target="_blank" title="https://www.meteobridge.com/wiki/index.php/Meteobridge_PRO">Meteobridge Pro</a>
-<br><img src="img/weather34logo.svg" width='60px' alt="weather34 logo"></span>
-</p>
+<a href="https://www.meteobridge.com/wiki/index.php/Meteobridge_PRO" target="_blank" title="https://www.meteobridge.com/wiki/index.php/Meteobridge_PRO"><blue>Pro</blue></a>
+</div>
+
+<div class='close'>
+<a href="https://weather34.com/homeweatherstation/" target="_blank" title="https://weather34.com/homeweatherstation/"><img src="img/weather34-mbsmart-logo.svg" class="logo"></img></a>
+</div>
 </div>
 </div>
