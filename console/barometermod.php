dd6940af6e2eac0c8f2344e81dd36aadd519ce32
brian underdown <brian260865@gmail.com>
2019-12-04 18:45:48 +0300

    Rev 1.07A adds light or dark theme setup option for alternative layout

--- console/barometermod.php
+++ console/barometermod.php
@@ -43,27 +43,22 @@ valuetextba{font-size:1em}
 <div class="homeweathercompass2" >
 <div class="homeweathercompass-line2">
 <div class="weather34barometerarrowactual"></div>
-<div class="weather34barometerarrow24"></div>
-
+<div class="weather34barometerarrowmin"></div>
+<div class="weather34barometerarrowmax"></div>
 </div>
-<div class="text2">
-<?php if ($weather["barometer_trend"]>20){echo '<div class="pressuretext"> <ogreen>'.$lang['Steady'].'</ogreen> </div>';}
-else if ($weather["barometer_trend"] < -1.3) {
-echo '<div class="pressuretextfast"><oblue>'.$lang['Falling'].' '.$lang['Rapidly'].'</oblue> <fall> '.$fallingsymbol.'</fall></div>';}
-else if ($weather["barometer_trend"] < 0) {
-echo '<div class="pressuretext"><oblue>'.$lang['Falling'].'</oblue> <fall> '.$fallingsymbol.'</fall></div>';}
-else if ($weather["barometer_trend"] > 1.3) {
-echo '<div class="pressuretextfast">'.$lang['Rising'].' '.$lang['Rapidly'].'<rise> '.$risingsymbol.' </rise></div>';}	  
-else if ($weather["barometer_trend"] > 0) {
-echo '<div class="pressuretext"> <oorange>'.$lang['Rising'].'</orange> <rise> '.$risingsymbol.' </rise></div>';}	  
-else echo '<div class="pressuretext"> <ogreen>'.$lang['Steady'].'</ogreen> </div>';?>
-<?php echo "<darkgrey>".$weather["barometer"],"</darkgrey>&nbsp;<span>".$weather["barometer_units"]."</span>";?>
+<div class="text2"><?php if ($weather["barometer_trend"]>20) {
+    echo '<div class="pressuretext"> <ogreen>Steady</ogreen> </div>';
+} elseif ($weather["barometer_trend"] < 0) {
+    echo '<div class="pressuretext"> <oblue>'.$lang['Falling'].'</oblue> <fall> '.$fallingsymbol.'</fall></div>';
+} elseif ($weather["barometer_trend"] > 0) {
+    echo '<div class="pressuretext"> <oorange>'.$lang['Rising'].'</orange> <rise> '.$risingsymbol.' </rise></div>';
+} else {
+    echo '<div class="pressuretext"> <ogreen>'.$lang['Steady'].'</ogreen> </div>';
+}?>
+<?php echo "<blue>".$weather["barometer"],"</blue>&nbsp;<span>".$weather["barometer_units"]."</span>";?>
 </div></div></div>
-<div class="barometerlimits"><div class='weather34-barometerruler'>
-<?php if ($weather["temp_units"]=='C'){echo "<weather34-barometerlimitmin><value>950</weather34-barometerlimitmin><weather34-barometerlimitmax><value>1050</weather34-barometerlimitmax>";}
-else echo "<weather34-barometerlimitminf><value>28</barometerlimitminf><weather34-barometerlimitmaxf><value>31</weather34-barometerlimitmaxf>";?></div></div></div></div></div></div>
-
-
+</span>
+</div></div>
 
 <div class="heatcircle3"><div class="heatcircle-content"><valuetextheading1>Max <?php echo $lang['Today']?></valuetextheading1>
 
