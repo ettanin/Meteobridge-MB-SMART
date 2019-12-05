7f72824b1342498d599a76a717d8d33b0637416b
brian underdown <brian260865@gmail.com>
2019-12-05 19:01:00 +0300

    Rev 1.07A adds App Icons

--- console/consoledavis.php
+++ console/consoledavis.php
@@ -12,9 +12,27 @@ include_once('console-settings.php');
 <meta name="mobile-web-app-capable" content="yes">
 <link rel="mask-icon" href="safari-pinned-tab.svg" color="#01a4b4">
 <meta name="apple-mobile-web-app-title" content="Weather34 Console">
-<meta name="application-name" content="Weather34 Console">
-<meta name="msapplication-TileColor" content="#f8f8f8">
+<meta name="application-name" content="<?php echo $stationName; ?>Console">
+<meta name="apple-mobile-web-app-title" content="<?php echo $stationName; ?> Console">
+<meta name="application-name" content="<?php echo $stationName; ?> Console">
+<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
+<meta name="msapplication-TileColor" content="#f8f8f8">
 <link href="console-<?php echo $theme1?>.css?version=<?php echo filemtime('console-'.$theme1.'.css')?>" rel="stylesheet prefetch">
 <link rel="preload" href="fonts/clock3-webfont.woff" as="font" type="font/woff" crossorigin>
 <link rel="preload" href="fonts/verbatim-regular.woff" as="font" type="font/woff" crossorigin>
