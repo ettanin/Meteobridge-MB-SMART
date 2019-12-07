aab1953f26c0f996cc5ab6e903215d7cb3239a09
brian underdown <brian260865@gmail.com>
2019-12-07 09:16:55 +0300

    Rev 1.07A finalize css layout and app icons for alternative view

--- console/consoledavis.php
+++ console/consoledavis.php
@@ -3,30 +3,30 @@ include_once('livedata.php');
 include_once('updater2.php');
 ?>
 <!DOCTYPE html><html><head>
-<title>Weather34 Sinanoba Istanbul </title>
-<meta name="title" content="Weather34 Sinanoba Istanbul">
-<meta name="description" content="Weather station providing current weather conditions for Sinanoba Istanbul">
+<title><?php echo $stationName;?> </title>
+<meta name="title" content="<?php echo $stationName;?>">
+<meta name="description" content="Providing current weather conditions for <?php echo $stationName?>">
 <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=yes">
 <meta name="mobile-web-app-capable" content="yes">
 <link rel="mask-icon" href="safari-pinned-tab.svg" color="#01a4b4">
 <meta name="apple-mobile-web-app-title" content="Weather34 Console">
 <meta name="application-name" content="Weather34 Console">
-<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="57x57" href="appicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="appicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="appicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="appicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="appicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="appicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="appicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="appicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="appicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="appicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="appicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="appicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="appicon/favicon-16x16.png">
 <link rel="manifest" href="site.webmanifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="appicon/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 <meta name="msapplication-TileColor" content="#f8f8f8">
 <link href="console-<?php echo $theme?>.css?version=<?php echo filemtime('console-'.$theme.'.css')?>" rel="stylesheet prefetch">
