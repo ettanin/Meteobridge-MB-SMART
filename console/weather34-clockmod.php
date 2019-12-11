f0fbf4f703e0e50c194ed748f15fdbfb9f7ff745
brian underdown <brian260865@gmail.com>
2019-12-11 13:45:48 +0300

    Rev 1.07A adds offline online wireless icon if data not updated within last 300 seconds

--- console/weather34-clockmod.php
+++ console/weather34-clockmod.php
@@ -1,12 +1,12 @@
-<?php 
-	
-include('livedata.php');header('Content-type: text/html; charset=utf-8');	?>
-<div class="clock">
+<?php include('livedata.php');header('Content-type: text/html; charset=utf-8');	?>
+<div class="online">
+<?php if(file_exists($livedata)&&time()- filemtime($livedata)>300)echo $wirelessoffline;else echo $wireless?>
+</div>
+  <div class="clock">
   <span id="hour"></span>
   <span id="minute"></span>
   <span id="second"></span> 
-</div> 
-  
+ </div>   
   <div class="thedate">
   <span id="year"></span>
   <span id="month"></span>
