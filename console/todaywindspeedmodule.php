d75964c919524fb3e46b5bb688ad6da241e34595
brian underdown <brian260865@gmail.com>
2019-12-14 15:11:33 +0300

    Rev 1.07A only for alternative console layout

--- console/todaywindspeedmodule.php
+++ console/todaywindspeedmodule.php
@@ -2,4 +2,4 @@
  <?php include_once('common.php');include('livedata.php');?>
  <div class="modulecaptionchart"><?php echo $lang['Wind']; ?> <?php echo $weather["wind_units"]; ?></div> 
  <iframe  class="charttempmodule" src="../weather34charts/todaywindspeedmodulechart2.php" frameborder="0" scrolling="no" width="320px" height="250px"></iframe>  
- <div class="legenddewpoint"><?php echo $lang['Wind']; ?></div>
+ <div class="legendtemp"><?php echo $lang['Windspeed']; ?></div>
