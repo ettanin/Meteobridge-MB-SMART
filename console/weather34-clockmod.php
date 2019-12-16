8f580556573d2978ebb25a3eeefb22017c2478bd
brian underdown <brian260865@gmail.com>
2019-12-16 17:08:07 +0300

    Rev 1.07A finalize alternative layout - added year/month data
    *please do not ask for anymore features for the alternative layout otherwise it wont be an alternative. you have the main template to serve additional data

--- console/weather34-clockmod.php
+++ console/weather34-clockmod.php
@@ -1,7 +1,9 @@
 <?php include('livedata.php');header('Content-type: text/html; charset=utf-8');	?>
 <div class="online">
 <?php if(file_exists($livedata)&&time()- filemtime($livedata)>300)echo $wirelessoffline;else echo $wireless?></div>
-<div class="indoorclock">  <?php echo $hometemp." " .$weather["temp_indoor"] . '<smalltempunit2>&deg;'.$weather["temp_units"] ?></smalltempunit2></div>  
+<div class="indoorclock">  <?php echo $hometemp." " .$weather["temp_indoor"] . '<smalltempunit2>&deg;'.$weather["temp_units"] ?></smalltempunit2>
+<?php if($weather["temp_indoor_trend"] >0)echo $risingsymbolsmall;else if($weather["temp_indoor_trend"]<0)echo $fallingsymbolsmall;?>
+</div>  
   <div class="clock">
   <span id="hour"></span>
   <span id="minute"></span>
