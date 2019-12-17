62f6af02464a190058de915fb4ed807748bcdeb9
brian underdown <brian260865@gmail.com>
2019-12-17 15:46:25 +0300

    Rev 1.07A alternative layout updated all languages + trend positions
    *please do not ask for anymore features for the alternative layout otherwise it wont be an alternative. you have the main template to serve additional data

--- console/consoledavis.php
+++ console/consoledavis.php
@@ -41,6 +41,7 @@ include_once('livedata.php');include_once('updater2.php');
 <link rel="preload" href="fonts/verbatim-regular.woff" as="font" type="font/woff" crossorigin>
 <link rel="preload" href="fonts/verbatim-medium.woff" as="font" type="font/woff" crossorigin>
 <link rel="preload" href="fonts/HelveticaNeue-Medium.woff" as="font" type="font/woff" crossorigin>
+<script>function pageLoaded() {document.querySelector("body").style.opacity = 1;}window.onload = pageLoaded;</script>
 </head>
 <body>
 <!-- weather34 NANOSD console flex layout -->
