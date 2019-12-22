e758eefb70c2dc70270f2dcb5f439def68d42a43
brian underdown <brian260865@gmail.com>
2019-12-22 13:45:53 +0300

    REV 1.07A improved toggle appearance main template

--- menu.php
+++ menu.php
@@ -11,36 +11,54 @@
 
 <div class="weather34admin"><header class="weather34admin__header">
 <div class="weather34topbarmail">
-<a href="bio.php" data-lity title="Contact WEATHERSTATION Info"><svg version="1.1" id="email icon weather34" width="24" height="24"  x="0px" y="0px" viewBox="0 0 419.284 419.284" stroke-width="1"><g>
-	<g><path fill="whitesmoke" stroke="whitesmoke" stroke-width="1" opacity="0.5" d="M363.284,126.616h-199.28c-6.352-45.063-48.032-76.444-93.095-70.092s-76.444,48.032-70.092,93.095
-c4.303,30.527,25.287,56.099,54.387,66.278v91.68c0,27.04,14.72,56,56,56h252.08c27.04,0,56-14.72,56-56v-125.04 C419.204,141.256,390.324,126.616,363.284,126.616z M165.044,138.456h198.24c9.825-0.036,19.446,2.8,27.68,8.16l-153.76,99.6 l-36.4-24c-0.643-0.586-1.408-1.023-2.24-1.28l-50.64-32.8C158.931,173.906,164.947,156.447,165.044,138.456z M12.004,138.136
-c0-38.881,31.519-70.4,70.4-70.4c38.881,0,70.4,31.519,70.4,70.4s-31.519,70.4-70.4,70.4 C43.541,208.492,12.048,176.999,12.004,138.136z M67.284,307.576h-0.08v-88.4c26.266,5.051,53.34-3.077,72.48-21.76l47.2,30.56 l-107.76,112C71.237,331.053,67.009,319.482,67.284,307.576z M362.564,351.736l-251.44-0.24c-7.604,0.192-15.152-1.34-22.08-4.48 l108.08-112l36.72,24h0.4c0.795,0.401,1.67,0.619,2.56,0.64c0.89-0.021,1.765-0.239,2.56-0.64h0.4l36.96-24l108,112 C377.759,350.153,370.203,351.763,362.564,351.736z M395.444,339.576l-107.76-112l112-72.56c5.268,8.093,7.869,17.633,7.44,27.28 v125.28C407.845,319.403,403.614,330.996,395.444,339.576z"/></g></g><g><g><path fill="#2f9dac" stroke="#aeb6bf" stroke-width="1" d="M132.241,108.322c-0.077-0.091-0.156-0.179-0.237-0.265c-2.205-2.474-5.997-2.692-8.471-0.488 c-0.003,0.003-0.006,0.005-0.009,0.008l-53.92,48l-27.28-38.64c-1.922-2.717-5.683-3.362-8.4-1.44 c-2.717,1.922-3.362,5.683-1.44,8.4l31.12,44.16c0.995,1.394,2.538,2.296,4.24,2.48h0.72c1.475,0.002,2.899-0.539,4-1.52 l58.96-52.24C134.057,114.64,134.378,110.854,132.241,108.322z"/></g></svg></a>
-&nbsp;&nbsp;&nbsp;
+<a href="bio.php" data-lity title="Contact WEATHERSTATION Info"><div class="weather34-toggle">
+<div class="circleyellow"></div>  
+<div class="tog yellow">Info</div>
+</div></a>
+
 <?php //metric/non-metric/uk
 if ($units!='us') {
-    echo '<a  href="./?units=us"> <metricgrey>US</metricgrey> <topbarimperialf>&deg;F</topbarimperialf>&nbsp;&nbsp;</a> ';
+    echo '<a  href="./?units=us"> <div class="weather34-toggle">
+	<div class="circlegreen"></div> 
+  
+   <div class="tog green">&deg;F</div>
+   </div></a>';
 }
 if ($units!='metric') {
-    echo '<a  href="./?units=metric"> <metricgrey>KM/H</metricgrey> <topbarmetricc>&deg;C</topbarmetricc>&nbsp;&nbsp;</a>';
+    echo '<a  href="./?units=metric"><div class="weather34-toggle">
+	<div class="circleblue"></div> 
+  
+   <div class="tog blue">&deg;C</div>
+   </div></a>';
 }
 if ($units!='uk') {
-    echo '<a  href="./?units=uk">  <metricgrey>UK</metricgrey> <topbarmetricc>&deg;C</topbarmetricc>&nbsp;&nbsp;</a>';
+    echo '<a  href="./?units=uk"> <div class="weather34-toggle">
+	<div class="circleyellow"></div> 
+  
+   <div class="tog yellow">UK</div>
+   </div></a>';
 }
 ?>
 </div>
 <weather34toptitle><?php echo $stationName; ?>&nbsp;Weather Station</weather34toptitle>
 <div class="weather34toolbar">
-<div class="weather34toolbar__right"><weather34thetime><?php echo $menuclocksvg; ?>&nbsp; <div id="theTime"></weather34thetime></div></div>
-<div class="weather34-theme-switch-wrapper"><div class="weather34-theme-switch">
-<a class="weather34-slider round" href=<?php if ($theme == 'dark') {
-    echo '?theme=light';
-} else {
-    echo '?theme=dark';
-}?>><?php if ($theme == 'dark') {
-    echo ' Light Mode';
-} else {
-    echo ' &nbsp;Dark Mode';
-}?> </a></div></div></div></div></div>
+<div class="weather34toolbar__right"><weather34thetime><?php echo $menuclocksvg;?>&nbsp; <div id="theTime"></weather34thetime></div></div>
+
+
+<a  href=<?php if ($theme == 'dark') { echo '?theme=light';} else {echo '?theme=dark';} ?>>
+      <?php
+        if ($theme == 'dark') {echo '<div class="weather34-toggle">
+          <div class="circlelight"></div> 
+        
+         <div class="tog light">Light</div>
+         </div>';} 
+        else {echo '<div class="weather34-toggle">
+          <div class="circledark"></div> 
+         <div class="tog dark">Dark</div>
+         </div>';}?></a>
+
+
+</div></div></div></div>
 <!-- start the weather34 pure css menu-->
 <nav>
  <!-- weather34 dot menu svg-->
