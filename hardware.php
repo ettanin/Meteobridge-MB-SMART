626fa831a161a4aea59843b5ff3c7de604934233
brian underdown <brian260865@gmail.com>
2019-12-29 14:49:07 +0300

    REV 2.0 for 2020 adds new styled alerts / can be switched off in setup
    *please note after January 1st this repo will not be updated please use the weather34 info page for updates thereafter. https://weather34.com/homeweatherstation/

--- hardware.php
+++ hardware.php
@@ -2,12 +2,6 @@
 <?php include_once('livedata.php');?>
 <div class="topframe">
 <div class="weather34thetime-small">&nbsp; <div id="theTime"></div></div>
-
-
-<?php //meteobridge and station hardware 
+<?php //meteobridge uptime
 $nanosduptime = $meteobridgeapi[81];function convert($nanosduptime){$weather34nanotimeago = "";$days1 = intval(intval($nanosduptime) / (3600*24));$hours1 = (intval($nanosduptime) / 3600) % 24;$minutes1 = (intval($nanosduptime) / 60) % 60;$seconds1 = (intval($nanosduptime)) % 60;if($days1> 1){$weather34nanotimeago .= "$days1 Days ";}else {if($days1>0){$weather34nanotimeago .= "$days1 Day ";}if($hours1 > 1){$weather34nanotimeago .= "$hours1 hrs ";}else if($hours1 >=0){$weather34nanotimeago .= "$hours1 hr ";}if($minutes1 > 1){$weather34nanotimeago .= "$minutes1 mins ";}else if($minutes1 >=0){$weather34nanotimeago .= "$minutes1 min ";}}return $weather34nanotimeago;}?>
-  <div class="nanouptime3" > NanoSD Uptime&nbsp;<oblue> <?php echo convert($nanosduptime)?></oblue></div>
-
-
-  </div>
-</div>
+<div class="nanouptime3" > <?php if ($mbplatform=="Pro"){echo "MB-".$mbplatform;}else echo "MB-".$mbplatform;?> Uptime&nbsp;<oblue> <?php echo convert($nanosduptime)?></oblue></div></div></div>
