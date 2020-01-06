ab2ead540836e8c711bfc44b1cd8ced3545f2122
brian underdown <brian260865@gmail.com>
2020-01-06 11:23:29 +0300

    REV 2.0 alternative layout adds rainfall bucket

--- console/rainmod.php
+++ console/rainmod.php
@@ -1,23 +1,29 @@
 <?php include('livedata.php');include('common.php');date_default_timezone_set($TZ);$raintimeago=$originalDate124;$seconds_ago = (time() - strtotime($raintimeago));?>
-<div class="modulecaption">
-<?php echo $lang['Rainfall']?> <blue1><?php echo $weather["rain_units"]?></blue1></div>
+<div class="modulecaption" style="margin-top:6px;">Rainfall <blue1><?php echo $weather["rain_units"]?></blue1></div>
+
 <div class="tempcontainer">
-<?php echo "<div class='maxdata' style='margin-left:10px'><blue>". $weather["rain_lasthour"]."</blue><smalltempunit4>&nbsp; ".$weather["rain_units"]."</smalltempunit4></div>";?> 
-<?php echo "<div class='mindata'><blue>".$weather["rain_rate"]."</blue><smalltempunit4>&nbsp;".$weather["rain_units"]."</smalltempunit4></div>";?>
-<?php echo "<div class='hidata'>".$lang['Last Hour']."</div>";?> 
-<?php echo "<div class='lodata'>Rate</div>";?>
-<?php //weather34 sez lets make the rain total for today look nice 
-if ($weather["rain_units"]=="mm"){
-echo '<div class=rainbox >'.number_format($weather['rain_today'],1).'<smalltempunit4> '.$weather["rain_units"].'</smalltempunit4>';}
-if ($weather["rain_units"]=="in"){
-echo '<div class=rainbox >'.number_format($weather['rain_today'],2).'<smalltempunit4> '.$weather["rain_units"].'</smalltempunit4>';}
-?>
-</div></smalltempunit></span></div>
-</div></div>
+<div class="weather34i-rairate-bar">
+<div id="raincontainer">
+<div id="weather34rainbeaker">
+<div id="weather34rainwater" style="height:<?php if ($weather["rain_units"] =='mm' && $weather["rain_today"]){echo $weather["rain_today"]*2.5+1;}
+else if ($weather["rain_units"] =='in' && $weather["rain_today"]){echo $weather["rain_today"]*25.4*2.5;}?>px;">      
+</div></div></div></div> 
+
+
+<div class="raincontainer1">
+<?php if ($weather["rain_units"] =='in'){	echo '<div class=raintoday1>'.number_format($weather["rain_today"],2)."<smallrainunit2b> ".$weather["rain_units"];}
+else if ($weather["rain_units"] =='mm' && $weather["rain_today"]<10){echo '<div class=raintoday1>'.number_format($weather["rain_today"],2)." <smallrainunit2b>".$weather["rain_units"];}
+  else if ($weather["rain_units"] =='mm'){
+  echo '<div class=raintoday1>'.number_format($weather["rain_today"],1)."<smallrainunit2b>".$weather["rain_units"];}
+?></smallrainunit2b></div></div>
 
 <div class="heatcircle"><div class="heatcircle-content">
-<?php  //last 24hrs
-echo "<valuetextheading1>".$lang['Last-Twenty-Four-Hour']."</valuetextheading1><br><div class=tempconverter1><div class=tempmodulehome0-5c ><blue>".$weather["rain_24hrs"]."</blue>&nbsp;<smalltempunit2>".$weather["rain_units"];
+<?php  //last 24 hours rainfall or current rain rate
+if ($weather["rain_rate"]>0){
+  echo "<valuetextheading1>".$lang['Rain']." Rate </valuetextheading1><br><div class=tempconverter1>
+  <div class=tempmodulehome0-5c ><blue>".$weather["rain_rate"]."</blue>&nbsp;<smalltempunit2>".$weather["rain_units"];
+}
+else echo "<valuetextheading1>".$lang['Last-Twenty-Four-Hour']." </valuetextheading1><br><div class=tempconverter1><div class=tempmodulehome0-5c ><blue>".$weather["rain_24hrs"]."</blue>&nbsp;<smalltempunit2>".$weather["rain_units"];
 ?><smalltempunit2></div></div></div>
 <div class="heatcircle2"><div class="heatcircle-content"><valuetextheading1><?php echo date('Y');?> <?php echo $lang['Total']?></valuetextheading1>
 <?php //rainfall year
@@ -29,7 +35,7 @@ echo "<div class=tempconverter1><div class=tempmodulehome0-5c><blue>". $weather[
 </smalltempunit2></div></div>
 
 <div class=theraingap>
-<div class=thetrendboxblue>
+<div class=thetrendboxblue style="margin-left:-120px">
 <?php
   // *rainfall hours or minutes ago if within last 24 hours
   // *rain yesterday if greater than 0 is displayed if no rain last 24 hours 
