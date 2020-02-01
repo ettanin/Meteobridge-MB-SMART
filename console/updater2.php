bedcddee61378850e289ad76112cf2528c4b38a2
brian underdown <brian260865@gmail.com>
2020-02-01 23:44:40 +0300

    REV 3.0 Alternative fix for 24hr/12hr clock ref Chuck...7 files ...

--- console/updater2.php
+++ console/updater2.php
@@ -17,15 +17,30 @@
 (function(a){a(document).ready(function(){a.ajaxSetup({cache:true,success:function(){a("#time-date").show()}});var c=a("#time-date");c.load("weather34-sunmod.php");var b=setInterval(function(){c.load("weather34-sunmod.php")},1300000)})})(jQuery);
 (function(a){a(document).ready(function(){a.ajaxSetup({cache:true,success:function(){a("#clock-date").show()}});var c=a("#clock-date");c.load("weather34-indoormod2.php");var b=setInterval(function(){c.load("weather34-indoormod2.php")},130000)})})(jQuery);
 </script>
+
 <script type="text/javascript">
 var clockID;var yourTimeZoneFrom='<?php echo $UTC?>';var d=new Date();
-var weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-var tzDifference=yourTimeZoneFrom*60+d.getTimezoneOffset();var offset=tzDifference*60*1000;function UpdateClock(){var e=new Date(new Date().getTime()+offset);var c=e.getHours();var a=e.getMinutes();var g=e.getSeconds();var f=e.getFullYear();var h=months[e.getMonth()];var b=e.getDate();var b=e.getDate();
+var weekdays=[];
+var months=[];
+//calculte the weather34 date-time UTC
+var tzDifference=yourTimeZoneFrom*60+d.getTimezoneOffset();
+var offset=tzDifference*60*1000;
+function UpdateClock(){
+var e=new Date(new Date().getTime()+offset);
+var a=e.getMinutes();
+var g=e.getSeconds();
+var f=e.getFullYear();
+var h=months[e.getMonth()];
+var b=e.getDate();
+<?php if($clockformat=='12') {echo "if(e.getHours()<12){amorpm=' am'}else{amorpm=' pm'}";} else {echo "amorpm='';";}?>
+// add the weather34 date prefix
 var suffix = "";switch(b) {case 1: case 21: case 31: suffix = 'st'; break;case 2: case 22: suffix = 'nd'; break;case 3: case 23: suffix = 'rd'; break;default: suffix = 'th';}
 var i=weekdays[e.getDay()];if(a<10){a="0"+a}if(g<10){g="0"+g}if(c<10){c="0"+c}
-document.getElementById("weather34clock4").innerHTML="<div class='thedate3'> "+i+" "+h+" "+b+suffix+" "+f+" </div><div class='clock3'><time> "+c+":"+a+":"+g}
-function StartClock(){clockID=setInterval(UpdateClock,500)}function KillClock(){clearTimeout(clockID)}window.onload=function(){StartClock()}(jQuery);</script></time>
-</div></div>
-
+//weather34 option to use 24/12 hour format
+var c=e.getHours()
+<?php if ($clockformat == '12') { echo '% 12 || 12';} else { echo '% 24 || 00';}?>;
+document.getElementById("weather34clock4").innerHTML="<div class='clock3'><time> "+c+":"+a+":"+g+
+"<?php if($clockformat=='12') {echo "".date('a')."";} else {echo "";}?>"}
+function StartClock(){clockID=setInterval(UpdateClock,500)}
+function KillClock(){clearTimeout(clockID)}window.onload=function(){StartClock()}(jQuery);</script></time>
 </div></div>
